2e5fea3bfb7136131cd891f9b8a42df4ad060841
Gunavanth Kakade <gunak746@gmail.com>
2025-06-20 03:07:54 +0530

    Update Lab0010_Keywords_Identifiers.java

--- LearningJavaATB12xProgram/src/ex_01_Java_Basics/Lab0010_Keywords_Identifiers.java
+++ LearningJavaATB12xProgram/src/ex_01_Java_Basics/Lab0010_Keywords_Identifiers.java
@@ -1,6 +1,6 @@
 package ex_01_Java_Basics;
 
-public class Lab00010_Keywords_Identifiers {
+public class Lab0010_Keywords_Identifiers {
     public static void main(String[] args) {
         System.out.println("Hi");
     System.out.println("Bye");
